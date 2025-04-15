d891500ccf0b322073db6bbdc70ac3b455854c3b
kysman2580 <165656455+kysman2580@users.noreply.github.com>
2025-04-15 13:47:47 +0900

    ys test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/UserInterface/Common/Header/Header";
 import Footer from "./components/UserInterface/Common/Footer/Footer";
 import Main from "./components/UserInterface/Main/Main";
+import LoginPage from "./components/UserInterface/Member/LoginPage/LoginPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Main />}></Route>
+        {/* <Route path="/loginPage" element={<LoginPage />} /> */}
       </Routes>
       <Footer />
     </>
