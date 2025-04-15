e79e32610877ebf0fc525bca35eae35a0c492638
taskcat1234 <1232168@naver.com>
2025-04-15 16:30:33 +0900

    맵 파일 스타일 1차 분리

---
+++ src/components/UserInterface/ChargingMap/ChargingMap.styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledMap1 = styled.div`
+  position: absolute;
+  background: rgba(255, 253, 253, 0.51);
+  color: rgb(0, 0, 0);
+  width: 100%;
+  height: 700px;
+  z-Index: 1000;
+  text-align: center;
+  line-height: 700px;
+  font-size: 64px;
+`;
+
+export const StyledMap2 = styled.div`
+  text-lign: center; 
+  marginTop: 10px; 
+  color: red;
+`;
