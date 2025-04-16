fc5fa89f9fb09f485c4e33250db7703858036bc7
typh01 <127132180+typh01@users.noreply.github.com>
2025-04-16 16:45:12 +0900

    20250416.16:44 NewsMain 페이지 이식 (백엔드에서 api요청을 보내 기본적으로는 오류 페이지가 뜨는 것이 맞음)

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import LoginPage from "./components/UserInterface/Member/LoginPage/LoginPage";
 import Notice from "./components/UserInterface/Board/Notice/Notice";
 import CarManagement from "./components/AdminInterface/RentCar/CarManagement/CarManagement";
 import InsertCar from "./components/AdminInterface/RentCar/CarManagement/InsertCar";
+import NewsMain from "./components/UserInterface/News/NewsMain";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
         <Route path="/longRentCar" element={<LongTermRentCar />}></Route>
         <Route path="/hotRentCar" element={<HotdealRentCar />}></Route>
         <Route path="/subRentCar" element={<SubscribeRentCar />}></Route>
+        <Route path="/newsMain" element={<NewsMain />}></Route>
         {/* 아래부터는 관리자페이지만 적자 */}
         <Route path="/adminCarManagement" element={<CarManagement />}></Route>
         <Route path="/insertCar" element={<InsertCar />}></Route>
