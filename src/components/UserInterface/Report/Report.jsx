588969a0de52ba49bdd9a1e4bfb8414b91d6cf63
taskcat1234 <1232168@naver.com>
2025-04-21 15:48:15 +0900

    중간 저장 유저 신고 확인 + 관리자 신고 확인 페이지 기초 구조

---
+++ src/components/UserInterface/Report/Report.jsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Report2 } from "./Report.styled";
+
+const Report = () => {
+  const navi = useNavigate();
+  const [reports, setReports] = useState([]);
+  const [page, setPage] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost/reports", {
+        params: {
+          page: page,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        setReports([...reports, ...response.data]);
+        if (response.data.length < 5) {
+          setHasMore(false);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [page]);
+
+  const handleMore = () => {
+    setPage((page) => page + 1);
+  };
+
+  return (
+    <>
+      <Report2>
+        {reports.map((report) => (
+          <div
+            key={report.boardNo}
+            onClick={() => navi(`/reports/${report.boardNo}`)}
+            style={{ clear: "both" }}
+          >
+            <div>저런 아직 만들지않은 항목입니다</div>
+          </div>
+        ))}
+        <div>
+          {hasMore && (
+            <div
+              onClick={handleMore}
+              style={{ margin: "auto", marginTop: "20px" }}
+            >
+              더보기
+            </div>
+          )}
+        </div>
+      </Report2>
+    </>
+  );
+};
+
+export default Report;
