937994e818c4edaf800d9fe31d1704fa0c07d473
kysman2580 <165656455+kysman2580@users.noreply.github.com>
2025-04-24 15:13:42 +0900

    로그인 로그아웃완료

--- src/components/UserInterface/Member/LoginPage/LoginPage.jsx
+++ src/components/UserInterface/Member/LoginPage/LoginPage.jsx
@@ -16,13 +16,56 @@ import {
 } from "./LoginPage.styles"
 import "./LoginPage.css"
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import axios from "axios";
+import { useAuth } from "../../Context/AuthContext/AuthContext";
 
 
 
 
 function LoginPage() {
-
     const navi = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { login } = useAuth();
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+
+
+        const loginData = {
+            email: email,
+            memberPw: password
+        }
+
+        axios.post('http://localhost:80/auth/login', loginData)
+            .then(response => {
+                const { email, memberName, memberNo, refreshToken, accessToken } = response.data;
+                login(email, memberName, memberNo, refreshToken, accessToken);
+
+                alert('로그인 성공~~');
+                window.location.href = "/";
+            })
+            .catch(error => {
+                console.error('로그인 실패 : ', error);
+                if (error.response) {
+                    alert(`회원가입 실패 : ${error.response.data.message || '오류가 발생했습니다.'}`);
+                } else {
+                    alert('로그인 중 오류가 발생했습니다. 다시 시도해주세요.');
+                }
+            });
+    };
+
 
 
 
@@ -32,16 +75,30 @@ function LoginPage() {
         <StyledForm className="forms" action="">
             <StyledLoginForm>
                 <StyledLogoDiv >
-                    <StyledImg src="/images/Logo.png" alt="전기충만 로고" onClick={() => navi("/")}/>
+                    <StyledImg src="/images/Logo.png" alt="전기충만 로고" onClick={() => navi("/")} />
                 </StyledLogoDiv>
 
                 <StyledInputContainer>
-                    <StyledInput className="email" type="email" name="name" autoComplete="username" placeholder="이메일" />
-                    <StyledInput className="password" type="password" name="password" autoComplete="current-password" placeholder="비밀번호" />
+                    <StyledInput
+                        className="email"
+                        type="email"
+                        name="name"
+                        value={email}
+                        onChange={handleEmailChange}
+                        autoComplete="username"
+                        placeholder="이메일" />
+                    <StyledInput
+                        className="password"
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                        autoComplete="current-password"
+                        placeholder="비밀번호" />
                 </StyledInputContainer>
 
                 <StyledFindDiv>
-                    <Styled_a href="">비밀번호 찾기</Styled_a>
+                    <Styled_a onClick={() => navi("/findByPwPage")}>비밀번호 찾기</Styled_a>
                     <a className="stick">|</a>
                     <Styled_a href="">아이디 찾기</Styled_a>
                     <a className="stick">|</a>
@@ -49,7 +106,7 @@ function LoginPage() {
                 </StyledFindDiv>
 
                 <StyledButtonDiv>
-                    <StyledLoginButton>
+                    <StyledLoginButton type="button" onClick={handleLogin}>
                         로그인
                     </StyledLoginButton>
                 </StyledButtonDiv>
