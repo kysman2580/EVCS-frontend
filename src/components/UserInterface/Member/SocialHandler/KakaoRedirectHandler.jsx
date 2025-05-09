21d503772da5f6a1dcbad1fd6235d1688ed5c17b
kysman2580 <165656455+kysman2580@users.noreply.github.com>
2025-05-09 14:36:44 +0900

    QA를 위한 임시 저장' :

--- src/components/UserInterface/Member/SocialHandler/KakaoRedirectHandler.jsx
+++ src/components/UserInterface/Member/SocialHandler/KakaoRedirectHandler.jsx
@@ -12,13 +12,15 @@ function KakaoRedirectHandler() {
         const nickname = decodeURIComponent(params.get("nickname"));
         const email = params.get("email");
         const memberNo = params.get("memberNo");
+        const accessToken = params.get("accessToken");
+        const refreshToken = params.get("refreshToken");
 
-        if (nickname && email && memberNo) {
+        if (nickname && email && memberNo && accessToken && refreshToken) {
             // 여기서는 토큰 저장 X
             toast.success(`${nickname}님, 로그인 성공!`);
 
             // 클라이언트 AuthContext에 이메일/닉네임 정도만 저장 (원하면)
-            login(email, nickname, memberNo);
+            login(email, nickname, memberNo, accessToken, refreshToken);
 
             window.location.href = "/";
         } else {
